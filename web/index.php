86e04aa3de1ebc8e1942fb15d0e7c544e80a4940
andrew5678999900 <147448261+andrew5678999900@users.noreply.github.com>
2024-12-16 16:15:23 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -48,8 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
